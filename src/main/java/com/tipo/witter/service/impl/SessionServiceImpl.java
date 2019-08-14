a8b4b629c7ba2b14ac7befbe200c20775f2894f2
plankton <liuyufeng789@qq.com>
2019-08-14 22:44:49 +0800

    登陆完善

--- src/main/java/com/tipo/witter/service/impl/SessionServiceImpl.java
+++ src/main/java/com/tipo/witter/service/impl/SessionServiceImpl.java
@@ -1,11 +1,15 @@
 package com.tipo.witter.service.impl;
 
+import com.tipo.witter.enums.ResultEnum;
 import com.tipo.witter.enums.RoleEnum;
 import com.tipo.witter.pojo.Account;
 import com.tipo.witter.pojo.Msg;
 import com.tipo.witter.service.SessionService;
 import com.tipo.witter.tool.BaseStatic;
 import com.tipo.witter.util.http.RequestUtil;
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.UnsupportedEncodingException;
@@ -17,16 +21,20 @@ import java.io.UnsupportedEncodingException;
  */
 @Service
 public class SessionServiceImpl implements SessionService {
-
+    @Autowired
+    StringEncryptor encryptor;
+    @Value("${admin.user}")
+    private String user;
+    @Value("${admin.pass}")
+    private String pass;
     @Override
     public Msg addSession(Account account) throws UnsupportedEncodingException {
-        if (account.isRemember()){
+     if (encryptor.decrypt(user).equals(account.getUsername())
+             &&encryptor.decrypt(pass).equals(account.getPassword())){
+         RequestUtil.login(account.getUsername(), BaseStatic.ADMIN_NAME, RoleEnum.ADMIN);
+         return Msg.success();
+     }
 
-        }
-        else {
-            RequestUtil.login(account.getUsername(), BaseStatic.ADMIN_NAME, RoleEnum.ADMIN);
-        }
-
-        return null;
+        return Msg.fail(ResultEnum.PARAM_NOT_SUIT,"用户名或密码错误");
     }
 }
